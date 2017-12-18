a57ce10afa22bfd9ce248833c93f480b44dcd4fc
Nuno Godinho <mail@nunogodinho.com>
2017-12-18 16:24:53 +0000

    Player manager now tracks player termination and implements stop.

--- player/player_manager.py
+++ player/player_manager.py
@@ -2,6 +2,7 @@
 # vim: ts=4:sw=4:et
 
 from twisted.internet import defer
+from twisted import logger
 
 from .dbus_manager import OMXPlayerDBusManager
 from .player import OMXPlayer
@@ -12,6 +13,7 @@ class PlayerManager(object):
 
     def __init__(self, reactor, settings):
 
+        self.log = logger.Logger(namespace='player-manager')
         self.reactor = reactor
         self.dbus_mgr = OMXPlayerDBusManager(
             reactor,
@@ -44,10 +46,26 @@ class PlayerManager(object):
             return
 
         new_player = OMXPlayer('../videos/1-01.mkv', self.dbus_mgr, layer=20, fadein=1, fadeout=1)
-        yield new_player.spawn()
+        yield new_player.spawn(end_callable=lambda exit_code: self._player_ended(n))
         if self.current_player:
             yield self.current_player.stop()
         self.current_level = n
         self.current_player = new_player
 
+
+    def _player_ended(self, level):
+
+        self.log.info('player level={l!r} ended', l=level)
+        self.current_level = 0
+        self.current_player = None
+
+
+    @defer.inlineCallbacks
+    def stop(self):
+
+        if self.current_player:
+            self.log.info('stopping player level={l!r}', l=self.current_level)
+            yield self.current_player.stop()
+        self.log.info('stopping base player')
+        yield self.base_player.stop()
 
