650deb14aceeb8b19747fe7ac95aeb11bc6a4d4e
Tiago Montes <tiago.montes@gmail.com>
2017-12-22 11:29:24 +0000

    Better argument name for PlayerManager.level: new_level instead of n.

--- player/player_manager.py
+++ player/player_manager.py
@@ -107,14 +107,14 @@ class PlayerManager(object):
 
 
     @defer.inlineCallbacks
-    def level(self, n):
+    def level(self, new_level):
 
-        if n < self.current_level:
+        if new_level < self.current_level:
             return
 
-        player = self.players.get(n)
+        player = self.players.get(new_level)
         if player:
-            self.current_level = n
+            self.current_level = new_level
             player.play()
 
 
