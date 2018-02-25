879ee7aa97e45f4f2d906b275aad2826e39cb0a9
Tiago Montes <tiago.montes@gmail.com>
2018-02-25 19:36:28 +0000

    Audio input: now spawning and terminating arecord sub-process.

--- inputs/audio/input.py
+++ inputs/audio/input.py
@@ -13,6 +13,7 @@ from twisted.internet import defer
 from twisted import logger
 
 from inputs import input_base
+from common import process
 
 
 
@@ -31,19 +32,73 @@ class AudioInput(input_base.InputBase):
                  device, channels, format, rate, buffer_time, respawn_delay):
 
         super(AudioInput, self).__init__(reactor, event_manager)
+        self._spawn_args = [
+            nice_bin,
+            arecord_bin,
+            '--device=%s' % (device,),
+            '--channels=%s' % (channels,),
+            '--format=%s' % (format,),
+            '--rate=%s' % (rate,),
+            '--buffer-size=%s' % (buffer_time,),
+        ]
+        self._respawn_delay = respawn_delay
+        self._arecord_proto = None
 
 
     @defer.inlineCallbacks
     def start(self):
 
+        _log.info('starting')
+        yield self._spawn_arecord()
         _log.info('started')
-        yield defer.succeed(None)
+
+
+    @defer.inlineCallbacks
+    def _spawn_arecord(self):
+
+        _log.debug('spawning arecord')
+        self._arecord_proto = process.spawn(
+            self._reactor,
+            self._spawn_args,
+            'inputs.audio.proc',
+        )
+        _log.debug('waiting arecord start')
+        yield self._arecord_proto.started
+        _log.debug('arecord started')
+
+        self._arecord_proto.stopped.addCallback(self._arecord_stopped)
+
+
+    def _arecord_stopped(self, exit_code):
+
+        _log.warn('arecord stopped')
+        self._arecord_proto = None
 
 
     @defer.inlineCallbacks
     def stop(self):
 
+        _log.info('stopping')
+
+        if not self._arecord_proto:
+            _log.info('no arecord process to stop')
+            return
+
+        _log.info('signalling arecord termination')
+        try:
+            self._arecord_proto.terminate()
+        except OSError as e:
+            _log.warn('signalling arecord failed: {e!r}', e=e)
+            raise
+        else:
+            _log.info('signalled arecord termination')
+
+        _log.debug('waiting for arecord termination')
+        yield self._arecord_proto.stopped
+        _log.debug('arecord terminated')
+
         _log.info('stopped')
+
         yield defer.succeed(None)
 
 
